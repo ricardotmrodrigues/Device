04e8b7dd6b6c6c2c44de53ed61ea9f684072531b
Ricardo Rodrigues <rtmrodrigues@gmail.com>
2026-01-31 17:50:22 +0000

    fast

--- Application/Extensions/ServiceCollectionExtensions.cs
+++ Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,22 @@
 using Application.Behaviors;
 using Application.CQRS;
 using FluentValidation;
+using Infrastructure.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    private const string ConnectionStringEnvVar = "CONNECTION_STRING";
+
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        var connectionString = Environment.GetEnvironmentVariable(ConnectionStringEnvVar)
+            ?? throw new InvalidOperationException($"Environment variable '{ConnectionStringEnvVar}' is not set.");
+
+        services.AddInfrastructure(connectionString);
+
         var assembly = typeof(ServiceCollectionExtensions).Assembly;
 
         services.AddValidatorsFromAssembly(assembly);
