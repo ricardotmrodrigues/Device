e098a2e607a867fba0daaf74071663875184db3c
Ricardo Rodrigues <rtmrodrigues@gmail.com>
2026-02-01 15:25:37 +0000

    wip

--- API/Program.cs
+++ API/Program.cs
@@ -53,7 +53,7 @@ public class Program
             app.UseSwaggerUI();
         // }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
 
